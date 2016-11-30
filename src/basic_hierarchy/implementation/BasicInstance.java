203c4352669193c62763363c3d06df57c0554657
kartoFlane <tbachminski@gmail.com>
2016-11-30 14:08:13 +0100

    Added convenience constructor w/o trueClass param to BasicInstance

--- src/basic_hierarchy/implementation/BasicInstance.java
+++ src/basic_hierarchy/implementation/BasicInstance.java
@@ -2,39 +2,55 @@ package basic_hierarchy.implementation;
 
 import basic_hierarchy.interfaces.Instance;
 
-public class BasicInstance implements Instance {
+
+public class BasicInstance implements Instance
+{
 	private String instanceName;
 	private double[] data;
 	private String nodeId;
 	private String trueClass;
-	
-	public BasicInstance(String instanceName, String nodeId, double[] data, String trueClass)
+
+
+	public BasicInstance( String instanceName, String assignedClass, double[] data )
 	{
 		this.instanceName = instanceName;
-		this.nodeId = nodeId;				
+		this.nodeId = assignedClass;
 		this.data = data;
+	}
+
+	public BasicInstance( String instanceName, String assignedClass, double[] data, String trueClass )
+	{
+		this( instanceName, assignedClass, data );
 		this.trueClass = trueClass;
 	}
+
 	@Override
-	public String getNodeId() {
+	public String getInstanceName()
+	{
+		return instanceName;
+	}
+
+	@Override
+	public double[] getData()
+	{
+		return data;
+	}
+
+	@Override
+	public String getNodeId()
+	{
 		return nodeId;
 	}
 
 	@Override
-	public double[] getData() {
-		return data;
+	public void setNodeId( String assignedClass )
+	{
+		this.nodeId = assignedClass;
 	}
+
 	@Override
-	public String getTrueClass() {
+	public String getTrueClass()
+	{
 		return trueClass;
-	}
-	@Override
-	public String getInstanceName() {
-		return instanceName;
-	}
-	@Override
-	public void setNodeId(String nodeId) {
-		this.nodeId = nodeId;
-		
 	}
 }
