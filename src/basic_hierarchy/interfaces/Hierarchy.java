7a70b5afc9b52a5df08b8e833109b0327e9fdcdc
kartoFlane <tbachminski@gmail.com>
2016-11-30 14:01:22 +0100

    Added documentation to interfaces.

--- src/basic_hierarchy/interfaces/Hierarchy.java
+++ src/basic_hierarchy/interfaces/Hierarchy.java
@@ -1,12 +1,54 @@
 package basic_hierarchy.interfaces;
 
-public interface Hierarchy {
+/**
+ * A hierarchy is a collection of {@link Node}s, all of which share one common ancestor (the root group).
+ */
+public interface Hierarchy
+{
+	/**
+	 * Returns the root node of this hierarchy.
+	 */
 	public Node getRoot();
+
+	/**
+	 * Returns an array of groups of instances.
+	 */
 	public Node[] getGroups();
+
+	/**
+	 * Returns the number of groups in this hierarchy. Equivalent to {@code getGroups().length}.
+	 */
 	public int getNumberOfGroups();
+
+	/**
+	 * Returns an array of classes present in this hierarchy.
+	 */
 	public String[] getClasses();
+
+	/**
+	 * Returns the number of ground-truth classes. Equivalent to {@code getClasses().length}.
+	 */
 	public int getNumberOfClasses();
-	public int getClassCount(String className, boolean withClassHierarchy);
+
+	/**
+	 * Returns the total number of instances in this hierarchy, ie. sum of instances in all groups.
+	 */
 	public int getNumberOfInstances();
+
+	/**
+	 * Returns the number of groups within the specified class.
+	 * 
+	 * @param className
+	 *            Class name to look for.
+	 * @param withNodeInheritance
+	 *            if true, the result will also include child classes of the specified class.
+	 */
+	public int getClassCount( String className, boolean withNodeInheritance );
+
 	public void printTree();
+
+	/**
+	 * @return names for each data column in this hierarchy
+	 */
+	public String[] getDataNames();
 }
--- src/basic_hierarchy/interfaces/Node.java
+++ src/basic_hierarchy/interfaces/Node.java
@@ -2,36 +2,94 @@ package basic_hierarchy.interfaces;
 
 import java.util.LinkedList;
 
-public interface Node {	
+
+/**
+ * A group is a collection of {@link Instance}s which belong to the same class.
+ */
+public interface Node
+{
+	/**
+	 * @return the id of this group.
+	 */
 	public String getId();
+
+	/**
+	 * @return the parent group of this group, or null if this is the root group.
+	 */
 	public Node getParent();
+
+	/**
+	 * @return the parent group's id, or null if this is the root group.
+	 */
 	public String getParentId();
+
+	/**
+	 * @return the child groups of this group.
+	 */
 	public LinkedList<Node> getChildren();
-	
-	/*
-	 * Should returns instances belonging to this particular node
+
+	/**
+	 * Returns instances which belong to this particular group.
 	 */
 	public LinkedList<Instance> getNodeInstances();
-	/*
-	 * Should returns instances belonging to this node and its predecessors 
+
+	/**
+	 * Returns instances which belong to this group or its subgroups.
 	 */
 	public LinkedList<Instance> getSubtreeInstances();
-	/*
-	 * Could be node centroid or medoid
+
+	/**
+	 * Returns a node centroid or medoid.
 	 */
 	public Instance getNodeRepresentation();
 
-	public void setParent(Node parent);
+	/**
+	 * Sets the parent node of this node.
+	 * 
+	 * @param parent
+	 *            the new parent node
+	 */
+	public void setParent( Node parent );
 
-	public void setChildren(LinkedList<Node> children);
+	/**
+	 * Sets the children nodes of this node.
+	 * 
+	 * @param children
+	 *            list of children nodes
+	 */
+	public void setChildren( LinkedList<Node> children );
 
-	public void addChild(Node child);
+	/**
+	 * Adds a child node to this node.
+	 * 
+	 * @param child
+	 *            the node to add
+	 */
+	public void addChild( Node child );
 
-	public void addInstance(Instance instance);
+	/**
+	 * Adds a new instance to this node.
+	 * 
+	 * @param instance
+	 *            the instance to add
+	 */
+	public void addInstance( Instance instance );
 
-	public void setInstances(LinkedList<Instance> instances);
+	/**
+	 * Sets the instance list of this node.
+	 * 
+	 * @param instances
+	 *            list of instances
+	 */
+	public void setInstances( LinkedList<Instance> instances );
 
-	public void setRepresentation(Instance representation);
+	/**
+	 * Sets the representation instance of this node.
+	 * 
+	 * @param representation
+	 *            representation instance
+	 */
+	public void setRepresentation( Instance representation );
 
 	public void printSubtree();
 }
