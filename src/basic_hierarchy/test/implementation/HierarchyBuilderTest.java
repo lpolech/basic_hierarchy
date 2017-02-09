e52453f4855c1aeada68ca2052c4e51eb5a111ef
kartoFlane <tbachminski@gmail.com>
2017-02-09 21:33:22 +0100

    Tweaked HierarchyBuilderTest to cover more cases, made is slightly more robust

--- src/basic_hierarchy/test/implementation/HierarchyBuilderTest.java
+++ src/basic_hierarchy/test/implementation/HierarchyBuilderTest.java
@@ -26,9 +26,10 @@ public class HierarchyBuilderTest
         root = new BasicNode( Constants.ROOT_ID, null, false );
 
         nodes.add( root );
-        nodes.add( new BasicNode( root.getId() + ".0.1", null, false ) );
-        nodes.add( new BasicNode( root.getId() + ".0.0.3", null, false ) );
-        nodes.add( new BasicNode( root.getId() + ".0.0.5", null, false ) );
+        nodes.add( new BasicNode( "gen.0.0.10", null, false ) );
+        nodes.add( new BasicNode( "gen.0.0.11.3", null, false ) );
+        nodes.add( new BasicNode( "gen.0.0.11.5", null, false ) );
+    }
     }
 
     @Test
@@ -36,25 +37,25 @@ public class HierarchyBuilderTest
     {
         // Creates:
         // - gen.0.0
-        // - gen.0.0.0
+        // - gen.0.0.11
         List<BasicNode> artificial = HierarchyBuilder.fixDepthGaps( root, nodes, false );
 
         // Assert that no unexpected nodes have been created
-        Assert.assertEquals( artificial.size(), 2 );
+        Assert.assertEquals( 2, artificial.size() );
 
         BasicNode artificial0 = artificial.get( 0 ); // gen.0.0
-        BasicNode artificial1 = artificial.get( 1 ); // gen.0.0.0
+        BasicNode artificial1 = artificial.get( 1 ); // gen.0.0.11
 
-        BasicNode leaf0 = nodes.get( 1 ); // gen.0.0.1
-        BasicNode leaf1 = nodes.get( 2 ); // gen.0.0.0.3
-        BasicNode leaf2 = nodes.get( 3 ); // gen.0.0.0.5
+        BasicNode leaf0 = nodes.get( 1 ); // gen.0.0.10
+        BasicNode leaf1 = nodes.get( 2 ); // gen.0.0.11.3
+        BasicNode leaf2 = nodes.get( 3 ); // gen.0.0.11.5
 
         // Assert child -> parent relations
-        Assert.assertEquals( artificial0.getParent(), root );
-        Assert.assertEquals( artificial1.getParent(), artificial0 );
-        Assert.assertEquals( leaf0.getParent(), artificial0 );
-        Assert.assertEquals( leaf1.getParent(), artificial1 );
-        Assert.assertEquals( leaf2.getParent(), artificial1 );
+        Assert.assertEquals( root, artificial0.getParent() );
+        Assert.assertEquals( artificial0, artificial1.getParent() );
+        Assert.assertEquals( artificial0, leaf0.getParent() );
+        Assert.assertEquals( artificial1, leaf1.getParent() );
+        Assert.assertEquals( artificial1, leaf2.getParent() );
 
         // Assert parent -> child relations
         Assert.assertTrue( root.getChildren().contains( artificial0 ) );
@@ -69,23 +70,34 @@ public class HierarchyBuilderTest
     {
         // Creates:
         // - gen.0.0
-        // - gen.0.0.0
+        // - gen.0.0.11
         List<BasicNode> artificialDepth = HierarchyBuilder.fixDepthGaps( root, nodes, false );
 
         // Creates:
-        // - gen.0.0.0.0
-        // - gen.0.0.0.1
-        // - gen.0.0.0.2
-        // - gen.0.0.0.4
+        // - gen.0.0.[0-9]
+        // - gen.0.0.11.0
+        // - gen.0.0.11.1
+        // - gen.0.0.11.2
+        // - gen.0.0.11.4
         List<BasicNode> artificialBreadth = HierarchyBuilder.fixBreadthGaps( root, false );
 
-        BasicNode artificial1 = artificialDepth.get( 1 ); // gen.0.0.0
+        BasicNode artificial1 = artificialDepth.get( 1 ); // gen.0.0.11
 
-        Assert.assertEquals( artificialBreadth.size(), 4 );
-        Assert.assertTrue( artificial1.getChildren().containsAll( artificialBreadth ) );
+        Assert.assertEquals( 14, artificialBreadth.size() );
 
-        for ( BasicNode leaf : artificialBreadth ) {
-            Assert.assertEquals( leaf.getParent(), artificial1 );
+        // Compiling under Java 7, can't use lambdas...
+        List<BasicNode> gen11 = new ArrayList<BasicNode>();
+        for ( BasicNode n : artificialBreadth ) {
+            if ( n.getId().startsWith( "gen.0.0.0.11." ) )
+                gen11.add( n );
+        }
+
+        Assert.assertEquals( 4, gen11.size() );
+
+        Assert.assertTrue( artificial1.getChildren().containsAll( gen11 ) );
+
+        for ( BasicNode leaf : gen11 ) {
+            Assert.assertEquals( artificial1, leaf.getParent() );
         }
     }
 }
