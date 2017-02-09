51e550d24ceeeeb134a25b9231125e9696c6e79e
kartoFlane <tbachminski@gmail.com>
2017-02-09 19:27:52 +0100

    Changed NodeIdComparator to use default string compareTo() method
    The ordering we want happens to be the same as lexicographic ordering used by the default implementation, and the default compareTo() method is also about 3 times faster than our custom comparator.

--- src/basic_hierarchy/common/NodeIdComparator.java
+++ src/basic_hierarchy/common/NodeIdComparator.java
@@ -6,27 +6,24 @@ import basic_hierarchy.interfaces.Node;
 
 
 /**
- * Compares two nodes.
- * <p>
- * Implementation compares the two nodes' IDs using {@linkplain StringIdComparator}.
- * </p>
+ * Compares two nodes by comparing their ids.
  */
 public class NodeIdComparator implements Comparator<Node>
 {
-	private StringIdComparator idComparator = new StringIdComparator();
+    /**
+     * Note: this comparator imposes orderings that are <b>inconsistent with {@code equals}</b>.
+     * 
+     * @see Comparator#compare(Object, Object)
+     */
+    @Override
+    public int compare( Node o1, Node o2 )
+    {
+        String o1Id = o1.getId();
+        String o2Id = o2.getId();
 
-
-	/**
-	 * Note: this comparator imposes orderings that are <b>inconsistent with {@code equals}</b>.
-	 * 
-	 * @see Comparator#compare(Object, Object)
-	 */
-	@Override
-	public int compare( Node o1, Node o2 )
-	{
-		String o1Id = o1.getId();
-		String o2Id = o2.getId();
-
-		return idComparator.compare( o1Id, o2Id );
-	}
+        // Node IDs have ordering consistent with Strings' default lexicographic ordering.
+        // Thus, we can just use default Java implementation, which is also 3x faster than
+        // our specialized StringIdComparator
+        return o1Id.compareTo( o2Id );
+    }
 }
